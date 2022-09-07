dda0a9963bd395a23fa7cbc552a617f2e6bf8733
Aldair2005 <83721041+Aldair2005@users.noreply.github.com>
2022-09-07 09:38:29 -0300

    Add files via upload

---
+++ bpinton/home fondo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<link rel="stylesheet" href="./css2/styles.css">
+  <link rel="icon" href="./img/juan.png">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>   
+</head>
+<?php
+include 'm.html'
+?>      
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+</style>
+</body>
+<body>
+
+</body>
+</html>
