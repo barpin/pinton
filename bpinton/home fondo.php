0da5e11ca43d9c3281135391f6f63d73e2b47c88
0hoarne <lehorane@gmail.com>
2022-09-08 12:48:23 -0300

    a

--- bpinton/home fondo.php
+++ bpinton/home fondo.php
@@ -3,22 +3,21 @@
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<link rel="stylesheet" href="./css2/styles.css">
-  <link rel="icon" href="./img/juan.png">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>   
-</head>
-<?php
-include 'm.html'
-?>      
-<body>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="./css2/styles.css">
+    <link rel="icon" href="./img/juan.png">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet"> 
 
-</style>
-</body>
-<body>
-
-</body>
+  </head>
+    
+  <body>
+  <?php
+  include 'm.html';
+  ?>    
+  </body>
 </html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+include 'bpinton/home fondo.php';
